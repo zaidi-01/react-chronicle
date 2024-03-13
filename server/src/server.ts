bc8bece9a539004b5621b14e243db901357b7578
Syed Zaidi <26808641+zaidi-01@users.noreply.github.com>
2024-03-12 23:45:15 -0400

    feat: add api routes
    - Add posts API routes
    - Add home data API route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,59 @@
-import express from "express";
 import cors from "cors";
+import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+let POST_ID = 1;
+
 app.use(cors());
 app.use(express.json());
 
-// TODO: Add api routes
+// TODO: Replace with database connection
+const posts = [
+  {
+    id: POST_ID++,
+    title: "Post 1",
+    content: "This is the first post",
+  },
+  {
+    id: POST_ID++,
+    title: "Post 2",
+    content: "This is the second post",
+  },
+];
+
+// TODO: Add routes as global constants
+// TODO: Add routes in separate files
+// TODO: Add Post model
+
+app.get("/homeData", (_, res) => {
+  res.json({
+    message:
+      "Welcome to our website! Explore our blog for interesting articles.",
+  });
+});
+
+app.get("/post", (_, res) => {
+  res.json(posts);
+});
+
+app.post("/post", (req, res) => {
+  const { title, content } = req.body;
+
+  if (!title || !content) {
+    return res.status(400).json({ message: "Title and content are required" });
+  }
+
+  const newPost = {
+    id: POST_ID++,
+    title,
+    content,
+  };
+  posts.push(newPost);
+
+  res.json(newPost);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
