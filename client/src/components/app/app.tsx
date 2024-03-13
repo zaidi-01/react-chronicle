809cb7d0132e1504001c7a6a9783ee047f73177e
Syed Zaidi <26808641+zaidi-01@users.noreply.github.com>
2024-03-12 22:15:29 -0400

    feat: add navigation bar
    - Add navigation bar
    - Add Material UI package
    - Add react-router-dom package
    - Add page routes

--- client/src/components/app/app.tsx
+++ client/src/components/app/app.tsx
@@ -1,8 +1,43 @@
-import React from "react";
+import { AppBar, Box, Button } from "@mui/material";
+import {
+  NavLink,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import "./app.scss";
 
+// TODO: Add routes as shared constants
+const routes: Map<string, string> = new Map([
+  ["Home", "/"],
+  ["Blog", "/blog"],
+  ["New Post", "/new-post"],
+]);
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <AppBar position="static">
+          <Box sx={{ flexGrow: 1, display: "flex" }}>
+            {Array.from(routes.keys()).map((page) => (
+              <NavLink to={routes.get(page) as string} key={page}>
+                <Button sx={{ color: "white", display: "block" }}>
+                  {page}
+                </Button>
+              </NavLink>
+            ))}
+          </Box>
+        </AppBar>
+
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/blog" element={<div>Blog</div>} />
+          <Route path="/new-post" element={<div>New Post</div>} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
